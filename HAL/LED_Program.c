1ffaacfc51b5d8454aad3b61e9f15a817e0ea09c
Nada Hassan <engrnada@users.noreply.github.com>
2023-11-07 00:56:12 +0200

    Create LED_Program.c

---
+++ HAL/LED_Program.c
@@ -0,0 +1,71 @@
+
+/*
+*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< DIO_Program.c>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* Auther: Nada Hassan
+* Layer : HAL
+* SWC : LED Driver
+*
+*/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "DIO_interface.h"
+#include "LED_interface.h"
+
+
+void LED_VoidInit(LED_Adjusting LED_Signal){
+
+	DIO_enumSetPinDirection(LED_Signal.Port, LED_Signal.Pin, DIO_PIN_OUTPUT);
+
+
+}
+
+void LED_VoidOn(LED_Adjusting LED_Signal){
+
+
+		if(LED_Signal.Status == Active_High){
+
+			DIO_enumSetPinValue(LED_Signal.Port, LED_Signal.Pin, DIO_PIN_HIGH);
+
+		}
+		else if (LED_Signal.Status == Active_Low){
+			DIO_enumSetPinValue(LED_Signal.Port, LED_Signal.Pin, DIO_PIN_LOW);
+
+		}
+
+}
+
+void LED_VoidOf(LED_Adjusting LED_Signal){
+
+	if(LED_Signal.Status == Active_High){
+
+				DIO_enumSetPinValue(LED_Signal.Port, LED_Signal.Pin, DIO_PIN_LOW);
+
+			}
+			else if (LED_Signal.Status == Active_Low){
+				DIO_enumSetPinValue(LED_Signal.Port, LED_Signal.Pin, DIO_PIN_HIGH);
+
+			}
+}
+
+void LED_VoidToggle(LED_Adjusting LED_Signal){
+
+	DIO_enumTogglePinValue(LED_Signal.Port, LED_Signal.Pin);
+
+
+
+}
+void Delay_time(int a){
+	int add;
+	int time;
+	int i;
+	time = a * 1000;
+	for(i=0; i<time ; i++){
+		add*=i;
+		add++;
+		add++;
+
+	}
+
+}
